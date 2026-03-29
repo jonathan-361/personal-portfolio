d21693d4871fe164ff60437716eb5a7faaa8a1aa
JosueXD3940 <sanchezpucjosue@gmail.com>
2026-03-28 23:17:12 -0600

    family error 500
    familia de error 500,501,502,503

--- src/modules/core/routes/Router.tsx
+++ src/modules/core/routes/Router.tsx
@@ -8,6 +8,10 @@ import { Error401 } from "@/modules/auth/section/errors/error401";
 import { Error403 } from "@/modules/auth/section/errors/error403";
 import { Error404 } from "@/modules/auth/section/errors/error404";
 import { Error405 } from "@/modules/auth/section/errors/error405";
+import { Error500 } from "@/modules/auth/section/errors/error500";
+import { Error501 } from "@/modules/auth/section/errors/error501";
+import { Error502 } from "@/modules/auth/section/errors/error502";
+import { Error503 } from "@/modules/auth/section/errors/error503";
 
 
 
@@ -22,6 +26,10 @@ export default function AppRouter() {
       <Route path={paths.error403} element={<Error403 />} />
       <Route path={paths.error404} element={<Error404 />} />
       <Route path={paths.error405} element={<Error405 />} />
+      <Route path={paths.error500} element={<Error500 />} />
+      <Route path={paths.error501} element={<Error501 />} />
+      <Route path={paths.error502} element={<Error502 />} />
+      <Route path={paths.error503} element={<Error503 />} />
       
     </Routes>
   );
--- src/modules/core/routes/paths/path.ts
+++ src/modules/core/routes/paths/path.ts
@@ -9,6 +9,10 @@ const paths = {
   error403: "/error-403",
   error404: "/error-404",
   error405: "/error-405",
+  error500: "/error-500",
+  error501: "/error-501",
+  error502: "/error-502",
+  error503: "/error-503",
 };
 
 export default paths;
