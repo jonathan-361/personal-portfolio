2f28e52ca2498858c7e51f7ef876225eb9cfade8
jonathan-361 <jonathanlopezuc@gmail.com>
2026-04-13 20:03:55 -0600

    Fix load profile image

--- src/modules/core/context/AuthContext.tsx
+++ src/modules/core/context/AuthContext.tsx
@@ -1,6 +1,13 @@
-import { createContext, useContext, useState, type ReactNode } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  type ReactNode,
+} from "react";
 import type { User } from "@/modules/home/models/user.model";
 import { useUserStore } from "@/modules/core/store/user.store";
+import { userService } from "@/modules/core/services/user-services/user.services";
 
 interface AuthContextType {
   isAuthenticated: boolean;
@@ -16,27 +23,49 @@ export const AuthContext = createContext<AuthContextType | undefined>(
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const { setUser: setZustandUser, clearUser: clearZustandUser } =
     useUserStore();
+  const [user, setUserState] = useState<User | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  const [user, setUserState] = useState<User | null>(() => {
-    const savedUser = localStorage.getItem("auth_user");
-    return savedUser ? JSON.parse(savedUser) : null;
-  });
+  useEffect(() => {
+    const initAuth = async () => {
+      const token = localStorage.getItem("auth_token");
+      if (token) {
+        try {
+          const freshUser = await userService.getMe();
+          setUserState(freshUser);
+          setZustandUser(freshUser);
+          setIsAuthenticated(true);
+        } catch (error) {
+          console.error("Token inválido o expirado");
+          logout();
+        }
+      }
+      setLoading(false);
+    };
+    initAuth();
+  }, [setZustandUser]);
 
-  const [isAuthenticated, setIsAuthenticated] = useState(!!user);
-
-  const login = (token: string, userData: User) => {
+  const login = async (token: string) => {
     localStorage.setItem("auth_token", token);
-    localStorage.setItem("auth_user", JSON.stringify(userData));
 
-    setUserState(userData);
-    setZustandUser(userData);
-    setIsAuthenticated(true);
+    try {
+      const freshUser = await userService.getMe();
+
+      localStorage.setItem("auth_user", JSON.stringify(freshUser));
+      setUserState(freshUser);
+      setZustandUser(freshUser);
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.error("Error al obtener datos tras login:", error);
+      logout();
+    }
   };
 
   const logout = () => {
     localStorage.removeItem("auth_token");
     localStorage.removeItem("auth_user");
-
+    localStorage.removeItem("user-storage");
     setUserState(null);
     clearZustandUser();
     setIsAuthenticated(false);
@@ -44,14 +73,13 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
-      {children}
+      {!loading && children}
     </AuthContext.Provider>
   );
 };
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  if (!context)
-    throw new Error("useAuth debe usarse dentro de un AuthProvider");
+  if (!context) throw new Error("useAuth debe usarse dentro de AuthProvider");
   return context;
 };
